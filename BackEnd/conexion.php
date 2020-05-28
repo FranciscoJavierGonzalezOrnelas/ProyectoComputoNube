2f1dad6546f54fd6eabb29ac9f6de382e202013c
Francisco <paco_go09@hotmail.com>
2020-05-28 15:54:21 -0500

    Se creo conexion a base de datos

---
+++ BackEnd/conexion.php
@@ -0,0 +1,11 @@
+<?php
+$link = 'mysql:host=localhost;dbname=tienda';
+$usuario = 'root';
+$pass = 'root';
+
+try {
+    $pdo = new PDO($link, $usuario, $pass);
+} catch (PDOException $e) {
+    print "¡Error!" . $e->getMessage() . "<br/>";
+    die();
+}
